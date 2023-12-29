b4229053e52a659ae5e18ed358da3b5c269b8ab7
t63065488 <t63065488@gmail.com>
2023-12-29 16:24:48 +0000

    Remove debug, fix event retrieval

--- src/github_api.py
+++ src/github_api.py
@@ -8,10 +8,22 @@ class GitHubApi:
     g = Github(auth=auth)
     
     def get_events_filter_type_and_datetime(self, type, time):
-        events = self.get_events().get_page(0)
+        events = self.get_user_events_as_list()
         events = [e for e in events if e.type == type and e.created_at > time ]
         return events
     
     def get_events(self):
         return self.g.get_user(self.config.get("USERNAME", "")).get_events()
     
+    def get_user_events_as_list(self):
+        eventsList = []
+        events = self.g.get_user(self.config.get("USERNAME", "")).get_events()
+        i = 0
+        while True: 
+            toAppend = events.get_page(i)
+            if toAppend == []:
+                break
+            else:
+                eventsList.extend(toAppend)
+            i += 1
+        return eventsList
--- src/main.py
+++ src/main.py
@@ -7,10 +7,6 @@ def main(args):
     g = GitHubApi()
     time = datetime.datetime.now().astimezone(datetime.UTC) - datetime.timedelta(days=7)
     events = g.get_events_filter_type_and_datetime("PushEvent", time)
-    for event in events:
-        print(event.created_at)
-        print(event.payload)
-    print('do stuff')
     
 if __name__ == "__main__":
     main(sys.argv[1:])
