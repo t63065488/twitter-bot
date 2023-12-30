d57f19d73d0a1332054ad4e1032f8bc13c63fb94
t63065488 <t63065488@gmail.com>
2023-12-30 14:06:18 +0000

    Autopep8 formatting

--- src/github_api.py
+++ src/github_api.py
@@ -1,22 +1,23 @@
 from github import Github, Auth
 from dotenv import dotenv_values
 
+
 class GitHubApi:
     time_format = "%y/%m/%d %H:%M:%S"
     config = dotenv_values(".env.github")
     auth = Auth.Token(config.get("ACCESS_TOKEN", ""))
     g = Github(auth=auth)
-    
+
     def get_events_filter_type_and_datetime(self, type, time):
         events = self.get_user_events_as_list()
-        events = [e for e in events if e.type == type and e.created_at > time ]
+        events = [e for e in events if e.type == type and e.created_at > time]
         return events
-    
+
     def get_user_events_as_list(self):
         eventsList = []
         events = self.g.get_user(self.config.get("USERNAME", "")).get_events()
         i = 0
-        while True: 
+        while True:
             toAppend = events.get_page(i)
             if toAppend == []:
                 break
@@ -24,6 +25,6 @@ class GitHubApi:
                 eventsList.extend(toAppend)
             i += 1
         return eventsList
-    
+
     def get_repo_name(self, id: str) -> str:
         return self.g.get_repo(id).full_name
--- src/main.py
+++ src/main.py
@@ -6,20 +6,22 @@ import sys
 
 def main(args):
     g = GitHubApi()
-    time = datetime.datetime.now().astimezone(datetime.UTC) - datetime.timedelta(days=7)
+    time = datetime.datetime.now().astimezone(
+        datetime.UTC) - datetime.timedelta(days=7)
     events = g.get_events_filter_type_and_datetime("PushEvent", time)
     repo_ids = get_unique_repo_ids(events)
     repos = []
     for id in repo_ids:
-        repos.append((id, g.get_repo_name(id), [e.payload.get("commits") for e in events if e.payload.get('repository_id') == id]))
-        
-        
+        repos.append((id, g.get_repo_name(id), [e.payload.get(
+            "commits") for e in events if e.payload.get('repository_id') == id]))
+
+
 def get_unique_repo_ids(events):
     id_set = set()
     for event in events:
         id_set.add(event.payload.get('repository_id'))
     return id_set
-    
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
